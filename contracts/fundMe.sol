322bf6c5b603a03f293449abf08854b834c25db7
MartindeOrta <martindeorta@gmail.com>
2023-10-30 09:27:52 -0300

    okas

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -1,9 +1,13 @@
-//SPDX-License-interfier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
 contract FundMe{
+    mapping(address => uint256) public addressToAmountfunded;
+
     function fund() public payable{
+        addressToAmountfunded[msg.sender] += msg.value;
+
         
     }
 }
