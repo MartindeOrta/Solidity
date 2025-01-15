80f26b9c2074bca6fbf1611bcbcc9c41f80fc1f2
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2025-01-15 16:49:14 -0300

    Create subasta.sol

---
+++ subasta.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+/*tengo que crear una subasta que cada vez que un usuario pague 
+el que gane se lo nombre y a los que pierden se le devuleve el 98$
+laa subasta solamente se le puede parar el owner
+*/
