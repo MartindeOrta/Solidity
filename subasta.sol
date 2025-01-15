1295fab3f1ca4f567f7b044eabad6b0917ddf1cf
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2025-01-15 16:51:20 -0300

    Update subasta.sol

--- subasta.sol
+++ subasta.sol
@@ -3,4 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 /*tengo que crear una subasta que cada vez que un usuario pague 
 el que gane se lo nombre y a los que pierden se le devuleve el 98$
 laa subasta solamente se le puede parar el owner
+crear una subasta
+bid() payable
+termino subasta onlyOwner
+termino la subasta
+quien fue el ganador
+devolver dinero 
+podemos devolver el 98%
+
 */
